b63afe647e19fcf964836dac4e293a066f6e0f00
SOVANTHOEUN <sovanbunthoeunvoung@gmail.com>
2019-05-17 13:55:06 +0700

    merge with Pheaktra

--- src/main/java/com/wegarden/web/controller/OrderController.java
+++ src/main/java/com/wegarden/web/controller/OrderController.java
@@ -6,6 +6,7 @@ import com.wegarden.web.model.order.UserOrder;
 import com.wegarden.web.model.stock.Stock;
 import com.wegarden.web.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
