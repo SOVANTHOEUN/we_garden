12eb45294f6635ed5a256b982093c42d5164c859
SOVANTHOEUN <sovanbunthoeunvoung@gmail.com>
2019-05-17 09:09:15 +0700

    push for login

--- src/main/java/com/wegarden/web/configuration/SecurityConfig.java
+++ src/main/java/com/wegarden/web/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
 
 
 @EnableWebSecurity
